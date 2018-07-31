4d1d3ef2d9085d1e14d4c14f668872717e5ff7a7
Sidney So <buffalo704@hotmail.com>
2018-07-31 14:14:53 -0400

    add topics and topic page

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # learninglog
 Python Django project - Python Crash Course
+
+source ll_env/bin/activate
+
+python manage.py makemigrations learning_logs
+python manage.py migrate
+
+python manage.py createsuperuser
+
+python manage.py runserver
--- learning_log/urls.py
+++ learning_log/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from learning_logs.views import index
+
+#https://docs.djangoproject.com/en/1.11/ref/urls/#include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'', include(('learning_logs.urls', 'learning_logs'), namespace='learning_logs')),
 ]
